a93976684b851dce9ca1e4429eaff0f70a161a08
ashish <ashishmangla1991@gmail.com>
2022-06-12 22:01:57 +0530

    add new code for server

--- 04/app.js
+++ 04/app.js
@@ -8,10 +8,48 @@ const http = require('http'); //nodemodule for server
 const fs = require('fs'); //nodemodule for File System
 
 //create Server
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
 
     //req.url => for gettign the request url
-    console.log('aaaa', req.url);
+    //req.method => for gettign the method
+    console.log('Request was made: ', req.url, req.method);
+
+
+    //request.setHeader(name, value) =>Sets a single header value for headers object
+
+    //Serving Plain Text
+    //res.setHeader('Content-Type','text/plain');
+
+    //Serving HTML Text
+    res.setHeader('Content-Type', 'text/html');
+
+    // res.write('<head><title>this is test</title></head>');
+    // res.write(`<p>Hello Ashish</p>`); //write response from the server
+
+    //Serving HTML from the filesssytem
+    fs.readFile('./04/views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        }
+        else {
+            res.write(data)
+            res.end();
+        }
+
+
+
+    })
+
+
+    //res.end(); //requestend and send back to server
+
+
+    //serving plane text
+    //res.writeHead(200,{'Content-Type': 'text-plain'});
+
+
+
 });
 
 //server.listen(8000);
@@ -19,6 +57,6 @@ const server = http.createServer((req,res)=>{
 /*
 Parameters for listen function
 */
-server.listen(8000,'localhost',()=>{ //PortNumber, HostName and a callback function
+server.listen(8000, 'localhost', () => { //PortNumber, HostName and a callback function
     console.log('server is running on Port Number 8000')
 })
