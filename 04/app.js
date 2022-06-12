a8fdc39ac1e4c06f5f1df916e24975eb11ed0729
ashish <ashishmangla1991@gmail.com>
2022-06-12 20:54:27 +0530

    addnew code

--- 04/app.js
+++ 04/app.js
@@ -1,13 +1,24 @@
-//https module
+/*
 
+https module
 
-const http = require('http');
-const fs = require('fs'); 
+*/
 
+const http = require('http'); //nodemodule for server
+const fs = require('fs'); //nodemodule for File System
 
+//create Server
 const server = http.createServer((req,res)=>{
-console.log('aaaa', req.url);
+
+    //req.url => for gettign the request url
+    console.log('aaaa', req.url);
 });
 
+//server.listen(8000);
 
-server.listen(8000);
+/*
+Parameters for listen function
+*/
+server.listen(8000,'localhost',()=>{ //PortNumber, HostName and a callback function
+    console.log('server is running on Port Number 8000')
+})
